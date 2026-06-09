4204d1f43f11307291ec6b4e1933fbed222517f0
Andreas Hindborg <a.hindborg@kernel.org>
2026-06-09 21:08:29 +0200

    LIST: [PATCH v2 50/83] block: rust: move gendisk vtable construction to separate function
    Refactor the `GenDiskBuilder::build` method to move the `gendisk`
    vtable construction into a separate helper function. This prepares for
    adding zoned block device support which requires conditional vtable
    setup.
    
    Link: https://msgid.link/20260609-rnull-v6-19-rc5-send-v2-50-82c7404542e2@kernel.org
    [ahindborg: drop unused `NonNull` import from `gen_disk.rs`]
    [ahindborg: qualify `validate_block_size` call in rnull configfs]
    Signed-off-by: Andreas Hindborg <a.hindborg@kernel.org>

--- drivers/block/rnull/configfs.rs
+++ drivers/block/rnull/configfs.rs
@@ -181,7 +181,7 @@ impl configfs::AttributeOperations<1> for DeviceConfig {
         let text = core::str::from_utf8(page)?.trim();
         let value = text.parse::<u32>().map_err(|_| EINVAL)?;
 
-        GenDiskBuilder::validate_block_size(value)?;
+        GenDiskBuilder::<NullBlkDevice>::validate_block_size(value)?;
         this.data.lock().block_size = value;
         Ok(())
     }
