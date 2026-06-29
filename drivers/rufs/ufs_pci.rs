2c613cf86a06d674d64bd79be0d0b4101614c7c3
jaemyunglee <jaemyung.lee@samsung.com>
2026-06-29 18:56:33 +0900

    rufs: add PCI table

--- drivers/rufs/ufs_pci.rs
+++ drivers/rufs/ufs_pci.rs
@@ -17,11 +17,94 @@ mod ufs_uic;
 
 use ufs_host::UfsHost;
 
+#[derive(Clone, Copy)]
+enum UfsPciPlatform {
+    Qemu,
+    Samsung,
+    IntelCnl,
+    IntelEhl,
+    IntelLkf,
+    IntelAdl,
+    IntelMtl,
+}
+
+impl UfsPciPlatform {
+    const fn name(self) -> &'static str {
+        match self {
+            Self::Qemu => "qemu",
+            Self::Samsung => "samsung",
+            Self::IntelCnl => "intel-cnl",
+            Self::IntelEhl => "intel-ehl",
+            Self::IntelLkf => "intel-lkf",
+            Self::IntelAdl => "intel-adl",
+            Self::IntelMtl => "intel-mtl",
+        }
+    }
+}
+
 kernel::pci_device_table!(
     PCI_TABLE,
     MODULE_PCI_TABLE,
     <UfsPci as pci::Driver>::IdInfo,
-    [(pci::DeviceId::from_id(pci::Vendor::REDHAT, 0x0013), (),)]
+    [
+        // Match the PCI IDs handled by drivers/ufs/host/ufshcd-pci.c.
+        (
+            pci::DeviceId::from_id(pci::Vendor::REDHAT, 0x0013),
+            UfsPciPlatform::Qemu,
+        ),
+        (
+            pci::DeviceId::from_id(pci::Vendor::SAMSUNG, 0xc00c),
+            UfsPciPlatform::Samsung,
+        ),
+        (
+            pci::DeviceId::from_id(pci::Vendor::INTEL, 0x9dfa),
+            UfsPciPlatform::IntelCnl,
+        ),
+        (
+            pci::DeviceId::from_id(pci::Vendor::INTEL, 0x4b41),
+            UfsPciPlatform::IntelEhl,
+        ),
+        (
+            pci::DeviceId::from_id(pci::Vendor::INTEL, 0x4b43),
+            UfsPciPlatform::IntelEhl,
+        ),
+        (
+            pci::DeviceId::from_id(pci::Vendor::INTEL, 0x98fa),
+            UfsPciPlatform::IntelLkf,
+        ),
+        (
+            pci::DeviceId::from_id(pci::Vendor::INTEL, 0x51ff),
+            UfsPciPlatform::IntelAdl,
+        ),
+        (
+            pci::DeviceId::from_id(pci::Vendor::INTEL, 0x54ff),
+            UfsPciPlatform::IntelAdl,
+        ),
+        (
+            pci::DeviceId::from_id(pci::Vendor::INTEL, 0x7e47),
+            UfsPciPlatform::IntelMtl,
+        ),
+        (
+            pci::DeviceId::from_id(pci::Vendor::INTEL, 0xa847),
+            UfsPciPlatform::IntelMtl,
+        ),
+        (
+            pci::DeviceId::from_id(pci::Vendor::INTEL, 0x7747),
+            UfsPciPlatform::IntelMtl,
+        ),
+        (
+            pci::DeviceId::from_id(pci::Vendor::INTEL, 0xe447),
+            UfsPciPlatform::IntelMtl,
+        ),
+        (
+            pci::DeviceId::from_id(pci::Vendor::INTEL, 0x4d47),
+            UfsPciPlatform::IntelMtl,
+        ),
+        (
+            pci::DeviceId::from_id(pci::Vendor::INTEL, 0xd335),
+            UfsPciPlatform::IntelMtl,
+        ),
+    ]
 );
 
 struct UfsPci;
@@ -34,20 +117,26 @@ struct UfsPciData<'a> {
 }
 
 impl pci::Driver for UfsPci {
-    type IdInfo = ();
+    type IdInfo = UfsPciPlatform;
     type Data<'a> = UfsPciData<'a>;
     const ID_TABLE: pci::IdTable<Self::IdInfo> = &PCI_TABLE;
 
     fn probe<'a>(
         pdev: &'a pci::Device<Core<'_>>,
-        _info: &'a Self::IdInfo,
+        platform: &'a Self::IdInfo,
     ) -> impl PinInit<Self::Data<'a>, Error> + 'a {
         pin_init::pin_init_scope(move || {
             pr_info!(
-                "rufs: probe: vendor={} device=0x{:04x}",
+                "rufs: probe: platform={} vendor={} device=0x{:04x} subvendor=0x{:04x} subdevice=0x{:04x} class={} revision=0x{:02x}",
+                platform.name(),
                 pdev.vendor_id(),
                 pdev.device_id(),
+                pdev.subsystem_vendor_id(),
+                pdev.subsystem_device_id(),
+                pdev.pci_class(),
+                pdev.revision_id(),
             );
+
 
             pdev.enable_device_mem()?;
             pdev.set_master();
